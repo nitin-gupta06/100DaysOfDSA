6782060961f28ac23c5d32a7c4825ef5627f9426
nitin-gupta06 <guptanitin7770@gmail.com>
2026-02-01 15:29:42 +0530

    ques2

---
+++ Day-1/ques2.c
@@ -0,0 +1,62 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?*/
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    int nums[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &nums[i]);
+    }
+    int target;
+    printf("Enter the target sum: ");
+    scanf("%d", &target);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                printf("[%d, %d]\n", i, j);
+                return 0;
+            }
+        }
+    }
+    printf("No such indices found.\n");
+    return 1;
+}
