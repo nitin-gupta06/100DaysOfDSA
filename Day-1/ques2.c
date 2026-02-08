031f3d0086fc458b883431d4394596da217e8f21
nitin-gupta06 <guptanitin7770@gmail.com>
2026-02-08 18:21:38 +0530

    Day 2

---
+++ Day-1/ques2.c
@@ -0,0 +1,67 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+#include <stdio.h>
+
+int sum(int nums[], int target, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                printf("[%d, %d]\n", i, j);
+                return 0;
+            }
+        }
+    }
+    printf("No two numbers add up to the target.\n");
+    return -1; 
+}
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    int target;
+    printf("Enter the target value: ");
+    scanf("%d", &target);
+
+    sum(nums, target, n);
+    return 0;
+}
---
+++ Day-2/ques2.c
@@ -0,0 +1,62 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+#include <stdio.h>
+
+int maxProfit(int* prices, int pricesSize){
+    int minPrice = prices[0];
+    int maxProfit = 0;
+    
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            int profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+    
+    return maxProfit;
+}
+
+int main() {
+    int n;
+    printf("Enter the number of days: ");
+    scanf("%d", &n);
+
+    int prices[n];
+    printf("Enter the stock prices for each day: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &prices[i]);
+    }
+    int result = maxProfit(prices, n);
+    printf("Maximum profit: %d\n", result);
+    return 0;
+}
