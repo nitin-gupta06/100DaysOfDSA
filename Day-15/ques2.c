a13ad6fcef30502029f7a42439ab92abb1d8fc53
nitin-gupta06 <guptanitin7770@gmail.com>
2026-02-16 19:51:45 +0530

    Day-15

--- Day-15/ques2.c
+++ Day-15/ques2.c
@@ -1,3 +1,83 @@
 /*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 */
+
+#include <stdio.h>
+
+void setZeroes(int matrix[200][200], int m, int n) {
+    int firstRowZero = 0;
+    int firstColZero = 0;
+
+    // Check first column
+    for (int i = 0; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            firstColZero = 1;
+            break;
+        }
+    }
+
+    // Check first row
+    for (int j = 0; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            firstRowZero = 1;
+            break;
+        }
+    }
+
+    // Mark rows and columns
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // Zero rows
+    for (int i = 1; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Zero columns
+    for (int j = 1; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            for (int i = 1; i < m; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Zero first column
+    if (firstColZero) {
+        for (int i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    // Zero first row
+    if (firstRowZero) {
+        for (int j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+}
